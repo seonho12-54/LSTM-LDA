71d73cf9a3c1240dc146d64113661970b76d33ea
seonho <hikkcom40@gmail.com>
2026-07-27 15:36:23 +0900

    build: initialize Oil-Wise AI package

---
+++ src/oil_wise/__init__.py
@@ -0,0 +1,6 @@
+"""Oil-Wise AI: news-aware WTI forecasting."""
+
+from oil_wise.config import ExperimentConfig, TOPIC_LABELS, TOPIC_SEEDS
+
+__all__ = ["ExperimentConfig", "TOPIC_LABELS", "TOPIC_SEEDS"]
+__version__ = "0.1.0"
